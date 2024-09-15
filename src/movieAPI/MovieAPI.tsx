3cbbd95c6d273af465607d1881b5e058c36b813b
BxyHoT <bhynot@gmail.com>
2024-09-15 19:21:29 +0300

    попровил типизацию и перенес вызов апи на компонент

--- src/movieAPI/MovieAPI.tsx
+++ src/movieAPI/MovieAPI.tsx
@@ -39,54 +39,61 @@ export class MovieAPI {
 
     const resurce = await fetch(url, options);
 
+    if (!resurce.ok) {
+      throw new Error(`Ошибка ответа по урлу ${url}`);
+    }
     return await resurce.json();
   }
 
   async getAllMovies(page = 1) {
     return await this.getResurce(
       `https://api.themoviedb.org/3/search/movie?query=return&include_adult=false&language=en-US&page=${page}`
-    ).then((movies) =>
-      movies.results.map(
-        ({
-          id,
-          overview,
-          release_date,
-          vote_average,
-          popularity,
-          poster_path,
-          title,
-          genre_ids,
-        }: IMovie) => {
-          let date: Date | string;
-
-          if (release_date === "") {
-            date = "No date";
-          } else {
-            date = parseISO(release_date);
-            date = format(date, "MMMM d, yyyy");
-          }
-
-          let posterPath;
-
-          if (poster_path === null) {
-            posterPath = "";
-          } else {
-            posterPath = "https://image.tmdb.org/t/p/w500" + poster_path;
-          }
-
-          return {
+    )
+      .then((movies) =>
+        movies.results.map(
+          ({
             id,
+            overview,
+            release_date,
+            vote_average,
             popularity,
+            poster_path,
             title,
-            overview: this.getTrimOwerview(overview),
-            realiseDate: date,
-            voteAverage: vote_average,
-            posterPath: posterPath,
-            genreIds: genre_ids,
-          };
-        }
+            genre_ids,
+          }: IMovie) => {
+            let date: Date | string;
+
+            if (release_date === "") {
+              date = "No date";
+            } else {
+              date = parseISO(release_date);
+              date = format(date, "MMMM d, yyyy");
+            }
+
+            let posterPath;
+
+            if (poster_path === null) {
+              posterPath = "";
+            } else {
+              posterPath = "https://image.tmdb.org/t/p/w500" + poster_path;
+            }
+
+            return {
+              id,
+              popularity,
+              title,
+              overview: this.getTrimOwerview(overview),
+              realiseDate: date,
+              voteAverage: vote_average,
+              posterPath: posterPath,
+              genreIds: genre_ids,
+            };
+          }
+        )
       )
-    );
+      .catch((err) => {
+        console.error("Ошибка фетча ", err);
+      });
   }
 
   async getGenreArray() {
